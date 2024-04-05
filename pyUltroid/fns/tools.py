096773a4fe884e4effe0bd5387da11e14c23b9e4
ishu9805 <abhishek47903@gmail.com>
2024-04-05 15:08:29 +0530

    Update tools.py

--- pyUltroid/fns/tools.py
+++ pyUltroid/fns/tools.py
@@ -504,27 +504,6 @@ def order_points(pts):
     return rect
 
 
-def four_point_transform(image, pts):
-    try:
-        import cv2
-    except ImportError:
-        raise DependencyMissingError("This function needs 'cv2' to be installed.")
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-    dst = np.array(
-        [[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]],
-        dtype="float32",
-    )
-    M = cv2.getPerspectiveTransform(rect, dst)
-    return cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-
 # ------------------------------------- Telegraph ---------------------------------- #
 TELEGRAPH = []
 
@@ -781,18 +760,6 @@ class TgConverter:
             os.remove(file)
         return name
 
-    @staticmethod
-    def to_image(input_, name, remove=False):
-        try:
-            import cv2
-        except ImportError:
-            raise DependencyMissingError("This function needs 'cv2' to be installed.")
-        img = cv2.VideoCapture(input_)
-        ult, roid = img.read()
-        cv2.imwrite(name, roid)
-        if remove:
-            os.remove(input_)
-        return name
 
     @staticmethod
     async def convert(
