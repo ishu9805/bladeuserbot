e840f5bfcd3dbdcc8eb520d0f5953e2b1531070d
ishu9805 <abhishek47903@gmail.com>
2024-04-05 21:18:19 +0530

    Update helper.py

--- pyUltroid/fns/helper.py
+++ pyUltroid/fns/helper.py
@@ -249,38 +249,6 @@ async def bash(cmd, run_code=0):
 # Will add in class
 
 
-async def updater():
-    from .. import LOGS
-
-    try:
-        off_repo = Repo().remotes[0].config_reader.get("url").replace(".git", "")
-    except Exception as er:
-        LOGS.exception(er)
-        return
-    try:
-        repo = Repo()
-    except NoSuchPathError as error:
-        LOGS.info(f"`directory {error} is not found`")
-        Repo().__del__()
-        return
-    except GitCommandError as error:
-        LOGS.info(f"`Early failure! {error}`")
-        Repo().__del__()
-        return
-    except InvalidGitRepositoryError:
-        repo = Repo.init()
-        origin = repo.create_remote("upstream", off_repo)
-        origin.fetch()
-        repo.create_head("main", origin.refs.main)
-        repo.heads.main.set_tracking_branch(origin.refs.main)
-        repo.heads.main.checkout(True)
-    ac_br = repo.active_branch.name
-    repo.create_remote("upstream", off_repo) if "upstream" not in repo.remotes else None
-    ups_rem = repo.remote("upstream")
-    ups_rem.fetch(ac_br)
-    changelog, tl_chnglog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
-    return bool(changelog)
-
 
 # ----------------Fast Upload/Download----------------
 # @1danish_00 @new-dev0 @buddhhu
