ba9eed59f0819c2fc4de7f9461db4ca9a46ca44a
ishu9805 <abhishek47903@gmail.com>
2024-04-05 11:26:54 +0530

    Update imagetools.py

--- plugins/imagetools.py
+++ plugins/imagetools.py
@@ -1,9 +1,4 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 """
 ✘ Commands Available -
 
@@ -53,7 +48,7 @@
     Create a Pixelated Image..
 """
 import os
-
+import cv2
 from . import LOGS, con
 
 try:
