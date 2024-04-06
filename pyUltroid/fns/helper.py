f33209f65bb06dac66c57b42c15f1a7fc5aa7c29
ishu9805 <abhishek47903@gmail.com>
2024-04-06 14:33:38 +0530

    Update helper.py

--- pyUltroid/fns/helper.py
+++ pyUltroid/fns/helper.py
@@ -222,7 +222,21 @@ if run_as_module:
             f"{sys.executable} -m pip install --no-cache-dir -r requirements.txt"
         )
 
-
+async def bash(cmd, run_code=0):
+    """
+    run any command in subprocess and get output or error."""
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip() or None
+    out = stdout.decode().strip()
+    if not run_code and err:
+        if match := re.match("\/bin\/sh: (.*): ?(\w+): not found", err):
+            return out, f"{match.group(2).upper()}_NOT_FOUND"
+    return out, err
 
 async def uploader(file, name, taime, event, msg):
     with open(file, "rb") as f:
