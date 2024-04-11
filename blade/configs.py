73d446730d992fda9ce26d8a7256e808db8cb847
ishu9805 <abhishek47903@gmail.com>
2024-04-11 13:36:27 +0530

    Add files via upload

---
+++ blade/configs.py
@@ -0,0 +1,38 @@
+
+import sys
+
+from decouple import config
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+
+class Var:
+    # mandatory
+    API_ID = (
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+    )
+    API_HASH = (
+        sys.argv[2]
+        if len(sys.argv) > 2
+        else config("API_HASH", default="3029454a7c4e0cc538818a3bf7a58336")
+    )
+    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    
+    # extras
+    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
+    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+    HEROKU_API = config("HEROKU_API", default=None)
+    VC_SESSION = config("VC_SESSION", default=None)
+    ADDONS = config("ADDONS", default=False, cast=bool)
+    VCBOT = config("VCBOT", default=False, cast=bool)
+    # for railway
+    # for sql
+    DATABASE_URL = config("DATABASE_URL", default=None)
+    # for MONGODB users
+    MONGO_URI = config("MONGO_URI", default=None)
