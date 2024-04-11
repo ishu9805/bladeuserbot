832d4e2d4d288d41b2e48fd47ae9019491477e2e
ishu9805 <abhishek47903@gmail.com>
2024-04-11 12:47:33 +0530

    Update admins.py

--- assistant/manager/admins.py
+++ assistant/manager/admins.py
@@ -7,7 +7,7 @@ from blade import _ult_cache
 from . import *
 
 
-@bladeuserbot_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
+@blade_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
 async def dowj(e):
     replied = await e.get_reply_message()
     if replied:
