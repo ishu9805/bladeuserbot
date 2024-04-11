b77a95d037a8c23fe6e22af098913c072d3f95d8
ishu9805 <abhishek47903@gmail.com>
2024-04-11 13:34:40 +0530

    Add files via upload

---
+++ assistant/__init__.py
@@ -0,0 +1,27 @@
+from telethon import Button, custom
+
+from plugins import ATRA_COL, InlinePlugin
+from blade import *
+from blade import _ult_cache
+from blade._misc import owner_and_sudos
+from blade._misc._assistant import asst_cmd, callback, in_pattern
+from blade.fns.helper import *
+from blade.fns.tools import get_stored_file
+from strings import get_languages, get_string
+
+OWNER_NAME = blade_x_userbot_cmd.full_name
+OWNER_ID = blade_x_userbot_cmd.uid
+
+AST_PLUGINS = {}
+
+
+async def setit(event, name, value):
+    try:
+        udB.set_key(name, value)
+    except BaseException as er:
+        LOGS.exception(er)
+        return await event.edit("`Something Went Wrong`")
+
+
+def get_back_button(name):
+    return [Button.inline("« Bᴀᴄᴋ", data=f"{name}")]
---
+++ assistant/manager/__init__.py
@@ -0,0 +1,4 @@
+from blade._misc._decorators import blade_cmd
+from blade.fns.helper import inline_mention
+
+from .. import *
