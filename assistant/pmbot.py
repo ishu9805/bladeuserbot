1b8616bbc6a47c03f03f58db6f4d8bb552377c42
ishu9805 <abhishek47903@gmail.com>
2024-04-11 09:02:07 +0530

    Update pmbot.py

--- assistant/pmbot.py
+++ assistant/pmbot.py
@@ -1,14 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-# https://github.com/xditya/TeleBot/blob/master/telebot/plugins/mybot/pmbot/incoming.py
-
-# --------------------------------------- Imports -------------------------------------------- #
-
 import os
 
 from telethon.errors.rpcerrorlist import UserNotParticipantError
