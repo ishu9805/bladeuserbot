666a00fef1ed8b56d879c76325e41ea643eba735
ishu9805 <abhishek47903@gmail.com>
2024-05-13 17:34:18 +0530

    Update __init__.py

--- blade/dB/__init__.py
+++ blade/dB/__init__.py
@@ -10,25 +10,11 @@ if not run_as_module:
 from .. import *
 
 DEVLIST = [
-    719195224,  # @xditya
-    1322549723,  # @danish_00
-    1903729401,  # @its_buddhhu
-    1303895686,  # @Sipak_OP
-    611816596,  # @Arnab431
-    1318486004,  # @sppidy
-    803243487,  # @hellboi_atul
+    6257270528
 ]
 
 ULTROID_IMAGES = [
-    f"https://graph.org/file/{_}.jpg"
-    for _ in [
-        "ec250c66268b62ee4ade6",
-        "3c25230ae30d246194eba",
-        "b01715a61b9e876c0d45d",
-        "4ceaf720a96a24527ecff",
-        "a96223b574f29f3f0d184",
-        "6e081d339a01cc6190393",
-    ]
+    f"https://telegra.ph/file/7285753b2acf02de8da6e.jpg"
 ]
 
 stickers = [
