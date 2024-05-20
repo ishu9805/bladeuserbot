d93beed78afab2e87215ae2307f8fa1e22bb168d
ishu9805 <abhishek47903@gmail.com>
2024-05-20 09:41:18 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,12 +1,12 @@
 from telethon import Button, custom
 
 from plugins import ATRA_COL, InlinePlugin
-from blade import *
-from blade import _ult_cache
-from blade._misc import owner_and_sudos
-from blade._misc._assistant import asst_cmd, callback, in_pattern
-from blade.fns.helper import *
-from blade.fns.tools import get_stored_file
+from pyUltroid import *
+from pyUltroid import _ult_cache
+from pyUltroid._misc import owner_and_sudos
+from pyUltroid._misc._assistant import asst_cmd, callback, in_pattern
+from pyUltroid.fns.helper import *
+from pyUltroid.fns.tools import get_stored_file
 from strings import get_languages, get_string
 
 OWNER_NAME = blade_x_userbot_cmd.full_name
